8335ad61d996f0b92aae42eef9cdab082bad95aa
MrMoutan <s182312@student.dtu.dk>
2019-03-28 16:48:51 +0100

    Tweak after rebasing.

--- gdoom/network.py
+++ gdoom/network.py
@@ -17,26 +17,26 @@ class PolicyNet(nn.Module):
             nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
             nn.BatchNorm2d(64),
             nn.ELU())
-        # self.conv3 = nn.Sequential(
-        #     nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
-        #     nn.BatchNorm2d(128),
-        #     nn.ELU())
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm2d(128),
+            nn.ELU())
         self.fc1 = nn.Sequential(
-            nn.Linear(16*16*64, 512),
+            nn.Linear(8*8*128, 512),
             nn.ELU())
         self.fc2 = nn.Sequential(
-            nn.Linear(512, 3),
+            nn.Linear(512, 512),
             nn.ELU())
-        # self.fc3 = nn.Sequential(
-        #     nn.Linear(512, 3),
-        #     nn.ELU())
+        self.fc3 = nn.Sequential(
+             nn.Linear(512, 3),
+             nn.ELU())
 
 
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        # x = self.conv3(x)
+        x = self.conv3(x)
         x = x.view(x.shape[0], -1)
         x = self.fc1(x)
         x = self.fc2(x)
