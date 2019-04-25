c9a7a8b1939304d0f24b26c1da5366e2aac8ccde
Pierre Segonne <pierre.segonne@student.ecp.fr>
2019-04-25 13:36:36 +0200

    Update Simple Q Learning.

--- gdoom/net.py
+++ gdoom/net.py
@@ -2,6 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    if isinstance(m, (nn.Conv2d)):
+        nn.init.kaiming_normal_(m.weight, a=2) # https://arxiv.org/pdf/1502.01852v1.pdf & https://pytorch.org/docs/stable/_modules/torch/nn/init.html
+    elif isinstance(m, (nn.BatchNorm2d)):
+        #nn.init.constant_(m.weight, 1)
+        #nn.init.constant_(m.bias, 0)
+        pass
+    elif isinstance(m, (nn.Linear)):
+        pass
+
 class DeepQNet(nn.Module):
     '''
     Neural Net that approximates the Q value
@@ -16,6 +27,7 @@ class DeepQNet(nn.Module):
             nn.Conv2d(4, 32, kernel_size=5, stride=2, padding=2),
             nn.BatchNorm2d(32),
             nn.ELU())
+        self.conv1
         self.conv2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
             nn.BatchNorm2d(64),
